9e66fbf3deae227ca9c1b5faab34e883767fffc5
Roza Jiang <junzhujiang@gmail.com>
2021-04-24 09:49:18 +1000

    Complete setup for command line application

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,45 @@
 import { clear } from "console";
 import chalk from 'chalk';
 import figlet from 'figlet';
+import yargs from 'yargs';
+import findLongestSubsequence from './utils/findLongestSubsequence';
 
 clear();
 
 // Print out the application name
 console.log(
   chalk.yellow(
-    figlet.textSync('Find SubSequence', { horizontalLayout: 'full' })
+    figlet.textSync('Find MAX SubSequence', { horizontalLayout: 'full' })
   )
 )
+
+// Process command line text
+const processedContent = yargs.command(
+  "$0 <input string>",
+  "the default command"
+).argv
+
+
+// Print out output
+try {
+
+  findLongestSubsequence(processedContent.inputstring as string)
+  const longestSubsequnce = findLongestSubsequence(processedContent.inputstring as string);
+
+  console.log(
+    chalk.yellow(`Input: ${processedContent.inputstring}`)
+  );
+
+  console.log(
+    chalk.yellow(`Output: ${longestSubsequnce}`)
+  );
+
+} catch (error) {
+
+  console.log(
+    chalk.red(`Make sure the inputs are passed in double quotes eg("1 2 2") \n Output: ${error}`)
+  )
+
+}
+
 
