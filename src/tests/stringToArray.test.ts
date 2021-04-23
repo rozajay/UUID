a867b80eb99b16459a85e0e359a38d4914f482c6
Roza Jiang <junzhujiang@gmail.com>
2021-04-23 16:46:22 +1000

    Add support function string to array of numbers

---
+++ src/tests/stringToArray.test.ts
@@ -0,0 +1,30 @@
+import stringToArray from '../utils/stringToArray'
+import { testCase01 } from './testData'
+
+describe('Convert string to array of numbers', () => {
+
+    it('Test Case 01', () => {
+
+        expect(stringToArray(testCase01)).toEqual([6, 1, 5, 9, 2])
+
+    })
+
+    it('Single number', () => {
+
+        expect(stringToArray('2')).toEqual([2])
+
+    })
+
+    it.each(['', ' '])('Throws error attempting to convert array of numbers with empty', (inputString) => {
+
+        expect(() => stringToArray(inputString)).toThrow('Input must contain at least one number')
+
+    })
+
+    it.each(['a b c d e', 'a b 1 2 3.'])('Throws error attempting to convert array of numbers with invalid input', (inputString) => {
+
+        expect(() => stringToArray(inputString)).toThrow('Input must only contain numbers')
+
+    })
+
+})
---
+++ src/utils/stringToArray.ts
@@ -0,0 +1,20 @@
+
+
+const findLongestSubsequence = (inputString: string): number[] => {
+
+    if (inputString === ' ' || inputString === '') {
+        throw Error('Input must contain at least one number')
+    }
+
+    const onlyDigitsAndSpace = inputString.match(/^[0-9 ]+$/)
+
+    if (onlyDigitsAndSpace) {
+        const inputArray = inputString.split(' ').map((numberString => parseInt(numberString)))
+        return inputArray
+    }
+
+    throw Error('Input must only contain numbers')
+
+}
+
+export default findLongestSubsequence
